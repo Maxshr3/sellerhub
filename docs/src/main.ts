402174aa5d453de190ea672a8283443bcd0f5d65
kuzyanin.m.e <m.e.kuzyanin@tmn4.etagi.com>
2026-06-21 18:44:53 +0500

    Очистка

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,12 +1,1 @@
-import express from "express";
-
-const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("SellerHub API is running");
-});
-
-app.listen(3000, () => {
-  console.log("MAIN FILE IS RUNNING");
-});
+console.log("SellerHub starting...");
